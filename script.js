a8c063864cff7a50a43adea81e8b2b5e5e56a98c
Jessica <jessica2003mail@gmail.com>
2022-08-23 14:53:00 -0400

    Set up files

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+let grid = document.querySelector(".shelf");
+let input = new Book();
+myLibrary.push(input);
+let 
