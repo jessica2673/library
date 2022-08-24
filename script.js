aa5b83d99e025d7bf74f83d700ac779c9cbc1273
Jessica <jessica2003mail@gmail.com>
2022-08-23 22:02:27 -0400

    Added a form and button in HTML

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+    // do stuff here
+    myLibrary.push(book);
 }
 
-let grid = document.querySelector(".shelf");
-let input = new Book();
-myLibrary.push(input);
-let 
+function display(book) {
+    const grid = document.querySelector("#shelf");
+    const newBook = document.createElement("div");
+    newBook.classList.add("newBook");
+    newBook.textContent = book.title + " by " + book.author + ", " +
+                        book.pages + " pages and read is " + book.read;
+    grid.appendChild(newBook);
+}
+
+// let JaneEyre = new Book("Jane Eyre", "Charlotte Bronte", 300, true);
+// addBookToLibrary(JaneEyre);
+// display(JaneEyre);
+
+// let MobyDick = new Book("Moby Dick", "Herman Melville", 300, true);
+// addBookToLibrary(MobyDick);
+// display(MobyDick);
+
+
+const btn = document.querySelector("#create");
+btn.addEventListener("click", () => {
+
+});
+
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", () => {
+    let newBook = new Book();
+});
+
+
+
+
+
+
+
+
