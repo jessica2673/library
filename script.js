a855192c3a0c032909a17fd762a1a015a940d67d
Jessica <jessica2003mail@gmail.com>
2022-08-24 17:20:47 -0400

    Finished project.

--- script.js
+++ script.js
@@ -1,46 +1,91 @@
 let myLibrary = [];
+let count = 0;
+const grid = document.querySelector("#shelf");
 
 function Book(title, author, pages, read) {
-    // the constructor...
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.num = count;
+    count++;
 }
 
 function addBookToLibrary(book) {
-    // do stuff here
     myLibrary.push(book);
 }
 
 function display(book) {
-    const grid = document.querySelector("#shelf");
     const newBook = document.createElement("div");
     newBook.classList.add("newBook");
-    newBook.textContent = book.title + " by " + book.author + ", " +
-                        book.pages + " pages and read is " + book.read;
     grid.appendChild(newBook);
+    
+    const info = document.createElement("p");
+    info.classList.add("info");
+
+    if(book.read === "read") {
+        info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
+                        book.pages + " pages and read: ✅ ";
+    } else {
+        info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
+                        book.pages + " pages and read: ❌";
+    }
+    newBook.appendChild(info);
+
+    //create toggle for read or unread
+    const toggle = document.createElement("button");
+    toggle.classList.add("toggle");
+    toggle.textContent = "Change Read Status";
+    newBook.appendChild(toggle);
+
+    toggle.addEventListener("click", e => {
+        e.preventDefault();
+
+        if(book.read === "read") {
+            book.read = "unread";
+            info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
+            book.pages + " pages and read: ❌";
+        } else {
+            book.read = "read";
+            info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
+            book.pages + " pages and read: ✅";
+        }
+    });
+
+    //add delete button
+    const newBtn = document.createElement("button");
+    newBtn.classList.add("btn");
+    newBtn.textContent = "Delete";
+    newBook.appendChild(newBtn);
+
+    newBtn.addEventListener("click", e => {
+        e.preventDefault();
+
+        myLibrary.splice(newBook.count, 1);
+        grid.removeChild(newBook);
+    });
 }
 
-// let JaneEyre = new Book("Jane Eyre", "Charlotte Bronte", 300, true);
-// addBookToLibrary(JaneEyre);
-// display(JaneEyre);
+//Example book
+let JaneEyre = new Book("Jane Eyre", "Charlotte Bronte", 300, true);
+addBookToLibrary(JaneEyre);
+display(JaneEyre);
 
-// let MobyDick = new Book("Moby Dick", "Herman Melville", 300, true);
-// addBookToLibrary(MobyDick);
-// display(MobyDick);
+let form = document.querySelector('form');
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
+    let title = document.querySelector('input[name=title]').value;
+    let author = document.querySelector('input[name=author]').value;
+    let pages = document.querySelector('input[name=pages]').value;
+    let read = document.querySelector("input[name=read]:checked").value;
 
-const btn = document.querySelector("#create");
-btn.addEventListener("click", () => {
-
+    let newBook = new Book(title, author, pages, read);
+    addBookToLibrary(newBook);
+    display(newBook);
 });
 
 
-let form = document.querySelector("#form");
-form.addEventListener("submit", () => {
-    let newBook = new Book();
-});
 
 
 
