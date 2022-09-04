938094aff8e8da46e07afc0407cd36b58237e5d1
Jessica <jessica2003mail@gmail.com>
2022-09-04 15:28:40 -0400

    Converted this project to use class.

--- script.js
+++ script.js
@@ -1,79 +1,81 @@
 let myLibrary = [];
-let count = 0;
+let count;
 const grid = document.querySelector("#shelf");
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.num = count;
-    count++;
-}
+class Book {
+    count = 0;
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
-function display(book) {
-    const newBook = document.createElement("div");
-    newBook.classList.add("newBook");
-    grid.appendChild(newBook);
-    
-    const info = document.createElement("p");
-    info.classList.add("info");
-
-    if(book.read === "read") {
-        info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
-                        book.pages + " pages and read: ✅ ";
-    } else {
-        info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
-                        book.pages + " pages and read: ❌";
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.count = 0;
     }
-    newBook.appendChild(info);
 
-    //create toggle for read or unread
-    const toggle = document.createElement("button");
-    toggle.classList.add("toggle");
-    toggle.textContent = "Change Read Status";
-    newBook.appendChild(toggle);
+    addBookToLibrary() {
+        myLibrary.push(this);
+    }
 
-    toggle.addEventListener("click", e => {
-        e.preventDefault();
-
-        if(book.read === "read") {
-            book.read = "unread";
-            info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
-            book.pages + " pages and read: ❌";
+    display() {
+        const newBook = document.createElement("div");
+        newBook.classList.add("newBook");
+        grid.appendChild(newBook);
+        
+        const info = document.createElement("p");
+        info.classList.add("info");
+    
+        if(this.read === "read") {
+            info.textContent = (this.title).toUpperCase() + " by " + (this.author).toUpperCase() + ", " +
+            this.pages + " pages and read: ✅ ";
         } else {
-            book.read = "read";
-            info.textContent = (book.title).toUpperCase() + " by " + (book.author).toUpperCase() + ", " +
-            book.pages + " pages and read: ✅";
+            info.textContent = (this.title).toUpperCase() + " by " + (this.author).toUpperCase() + ", " +
+            this.pages + " pages and read: ❌";
         }
-    });
+        newBook.appendChild(info);
 
-    //add delete button
-    const newBtn = document.createElement("button");
-    newBtn.classList.add("btn");
-    newBtn.textContent = "Delete";
-    newBook.appendChild(newBtn);
+        const toggle = document.createElement("button");
+        toggle.classList.add("toggle");
+        toggle.textContent = "Change Read Status";
+        newBook.appendChild(toggle);
 
-    newBtn.addEventListener("click", e => {
-        e.preventDefault();
+        toggle.addEventListener("click", e => {
+            e.preventDefault();
 
-        myLibrary.splice(newBook.count, 1);
-        grid.removeChild(newBook);
+            if(this.read === "read") {
+                this.read = "unread";
+                info.textContent = (this.title).toUpperCase() + " by " + (this.author).toUpperCase() + ", " +
+                this.pages + " pages and read: ❌";
+            } else {
+                this.read = "read";
+                info.textContent = (this.title).toUpperCase() + " by " + (this.author).toUpperCase() + ", " +
+                this.pages + " pages                                                                and read: ✅";
+            }
+        });
 
-        if(myLibrary.length === 0) {
-            document.getElementById("shelf").style.padding = "0px";
-        }
-    });
+        const newBtn = document.createElement("button");
+        newBtn.classList.add("btn");
+        newBtn.textContent = "Delete";
+        newBook.appendChild(newBtn);
+
+        newBtn.addEventListener("click", e => {
+            e.preventDefault();
+
+            myLibrary.splice(newBook.count, 1);
+            grid.removeChild(newBook);
+
+            if(myLibrary.length === 0) {
+                document.getElementById("shelf").style.padding = "0px";
+            }
+        });
+    }
 }
+
 
 //Example book
 let JaneEyre = new Book("Jane Eyre", "Charlotte Bronte", 300, false);
-addBookToLibrary(JaneEyre);
-display(JaneEyre);
+JaneEyre.addBookToLibrary();
+JaneEyre.display();
 
 let form = document.querySelector('form');
 form.addEventListener('submit', e => {
@@ -85,9 +87,9 @@ form.addEventListener('submit', e => {
     let read = document.querySelector("input[name=read]:checked").value;
 
     let newBook = new Book(title, author, pages, read);
-    addBookToLibrary(newBook);
+    newBook.addBookToLibrary();
     document.getElementById("shelf").style.padding = "20px";
-    display(newBook);
+    newBook.display();
 });
 
 
